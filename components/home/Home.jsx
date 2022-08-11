079ad0b51ef768174e4f533508d3fbdf468fa07c
Carter Moore <carterjmoore02@gmail.com>
2022-08-11 20:04:54 -0300

    Home section

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,10 +1,18 @@
-import classes from './Home.module.css';
+import classes from './Home.module.scss';
 
 const Home = () => {
   return (
     <div className={classes.home}>
       <div className={'layout ' + classes['home-textbox']}>
-        <h1>Test</h1>
+        <h3 className={classes.hi}>Hi, I&apos;m</h3>
+        <h1 className={classes.carter}>Carter Moore.</h1>
+        <h1 className={classes.subheader}>A full stack software developer.</h1>
+        <p className={classes.paragraph}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
       </div>
     </div>
   );
