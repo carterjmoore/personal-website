185f46d7833dac7738c67ebaccfbd3124252429e
Carter Moore <carterjmoore02@gmail.com>
2022-08-17 20:06:16 -0300

    Slider is functional now

--- components/projects/ProjectSlide.jsx
+++ components/projects/ProjectSlide.jsx
@@ -1,7 +1,17 @@
+import Button from '../ui/Button';
 import classes from './ProjectSlide.module.scss';
 
-const ProjectSlide = () => {
-  return <div className={`border-highlight ${classes.container}`}></div>;
+const ProjectSlide = (props) => {
+  return (
+    <div className={`border-highlight ${classes.container}`}>
+      <h3>{props.project.name}</h3>
+      <div className={classes.info}></div>
+      <div className={classes.buttons}>
+        <Button onClick={props.onPrev}>Prev</Button>
+        <Button onClick={props.onNext}>Next</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectSlide;
--- components/projects/Slider.jsx
+++ components/projects/Slider.jsx
@@ -1,14 +1,58 @@
+import { useState } from 'react';
 import ProjectSlide from './ProjectSlide';
 import classes from './Slider.module.scss';
 
-const projects = [{ name: 'ImgLabs' }, { name: 'Reflex Revolution' }];
+const projects = [
+  { name: 'imglabs.io' },
+  { name: 'Reflex Revolution' },
+  { name: 'Gun Time' },
+];
 
 const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => {
+      return (prevIndex + 1) % projects.length;
+    });
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => {
+      return prevIndex !== 0 ? prevIndex - 1 : projects.length - 1;
+    });
+  };
+
   return (
     <div className={classes.container}>
-      <div className={classes.slider}>
+      <div
+        className={classes.slider}
+        style={{ transform: `translateX(calc(${currentIndex}*(-100%)))` }}
+      >
         {projects.map((project) => {
-          return <ProjectSlide key={project.name} project={project} />;
+          return (
+            <ProjectSlide
+              key={project.name}
+              project={project}
+              onNext={handleNext}
+              onPrev={handlePrev}
+            />
+          );
+        })}
+      </div>
+      <div className={classes.progress}>
+        {projects.map((project, index) => {
+          return (
+            <div
+              key={project.name}
+              className={classes['progress-circle']}
+              style={{
+                backgroundColor: `${
+                  index === currentIndex ? 'cyan' : 'transparent'
+                }`,
+              }}
+            ></div>
+          );
         })}
       </div>
     </div>
