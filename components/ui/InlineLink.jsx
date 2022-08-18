4a2db621c951adfe9d94393ba85ea55b952558ea
Carter-Moore <carter.moore@populusgs.com>
2022-08-18 17:11:47 -0300

    Inline link works now

--- components/ui/InlineLink.jsx
+++ components/ui/InlineLink.jsx
@@ -2,16 +2,14 @@ import classes from './InlineLink.module.scss';
 
 const InlineLink = (props) => {
   return (
-    <div
+    <a
       href={props.href}
       target="_blank"
       rel="noopener noreferrer"
-      className={classes.wrapper}
+      className={classes.inlineLink}
     >
-      <a href={props.href} className={classes.inlineLink}>
-        <span>{props.children}</span>
-      </a>
-    </div>
+      <span>{props.children}</span>
+    </a>
   );
 };
 
