7a065b779402259fca288e9c491f8a280f02d9f1
Carter-Moore <carter.moore@populusgs.com>
2022-08-23 15:14:34 -0300

    Bullets for populus

--- components/work/Work.jsx
+++ components/work/Work.jsx
@@ -19,7 +19,13 @@ const works = [
     href: 'https://www.populusplus.com/',
     title: 'Full Stack Developer Co-op',
     date: 'May - August 2022',
-    bullets: ['Something about Populus', 'Something else about Populus'],
+    bullets: [
+      "Developed several key features of the company's web app product from front to back using Angular and Spring Boot.",
+      'Used Angular paired with PrimeNG to craft intuitive front-end user experiences.',
+      'Leveraged Spring Boot with Java to construct a REST API, allowing users to perform various CRUD operations on several types of data.',
+      'Crafted SQL scripts to update and migrate data in a Postgres database.',
+      'Worked with a full Atlassian toolset to ensure our team abided to Agile development principles.',
+    ],
   },
 ].reverse();
 
