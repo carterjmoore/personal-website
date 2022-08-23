d8db0cd9a80c7d1ab750df590227966773fdab91
Carter-Moore <carter.moore@populusgs.com>
2022-08-23 13:27:57 -0300

    Added local link option for InlineLink

--- components/ui/InlineLink.jsx
+++ components/ui/InlineLink.jsx
@@ -1,15 +1,31 @@
+import { useRouter } from 'next/router';
 import classes from './InlineLink.module.scss';
 
 const InlineLink = (props) => {
+  const router = useRouter();
+
+  const onLocalLink = () => {
+    router.push(props.href);
+  };
+
   return (
-    <a
-      href={props.href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={classes.inlineLink}
-    >
-      <span>{props.children}</span>
-    </a>
+    <>
+      {!props.local && (
+        <a
+          href={props.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.inlineLink}
+        >
+          <span>{props.children}</span>
+        </a>
+      )}
+      {props.local && (
+        <a className={classes.inlineLink} onClick={onLocalLink}>
+          <span>{props.children}</span>
+        </a>
+      )}
+    </>
   );
 };
 
