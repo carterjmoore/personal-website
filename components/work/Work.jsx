4fd7656c7f880e6bcfa5375902e9cdd8264f7890
Carter Moore <carterjmoore02@gmail.com>
2022-08-24 22:29:09 -0300

    Export work data to its own file

--- components/work/Work.jsx
+++ components/work/Work.jsx
@@ -3,35 +3,10 @@ import ScrollTo from '../ui/ScrollTo';
 import SelectButton from './SelectButton';
 import SelectedWork from './SelectedWork';
 import classes from './Work.module.scss';
-
-const works = [
-  {
-    id: 0,
-    company: 'Mach85',
-    href: 'https://mach85.com/',
-    title: 'Junior Software Developer Co-op',
-    date: 'September - December 2021',
-    bullets: ['Something about Mach85', 'Something else about Mach85'],
-  },
-  {
-    id: 1,
-    company: 'Populus Global Solutions',
-    href: 'https://www.populusplus.com/',
-    title: 'Full Stack Developer Co-op',
-    date: 'May - August 2022',
-    bullets: [
-      "Developed several key features of the company's web app product from front to back using Angular and Spring Boot.",
-      'Created unit and integration tests for my features, using Jest for Angular and JUnit for Spring Boot.',
-      'Used Angular paired with PrimeNG to craft intuitive front-end user experiences.',
-      'Leveraged Spring Boot with Java to construct a REST API, allowing users to perform various CRUD operations on several types of data.',
-      'Crafted SQL scripts to update and migrate data in a Postgres database.',
-      'Worked with a full Atlassian toolset to ensure our team abided to Agile development principles.',
-    ],
-  },
-].reverse();
+import WORKS from './WorkData';
 
 const Work = () => {
-  const [selectedWork, setSelectedWork] = useState(works[0]);
+  const [selectedWork, setSelectedWork] = useState(WORKS[0]);
 
   const onWorkSelected = (work) => {
     setSelectedWork(work);
@@ -44,7 +19,7 @@ const Work = () => {
         <h2 className={`section-header`}>Work Experience</h2>
         <div className={`border-highlight ${classes.container}`}>
           <div className={classes.select}>
-            {works.map((work) => {
+            {WORKS.map((work) => {
               return (
                 <SelectButton
                   work={work}
---
+++ components/work/WorkData.jsx
@@ -0,0 +1,27 @@
+const WORKS = [
+  {
+    id: 0,
+    company: 'Mach85',
+    href: 'https://mach85.com/',
+    title: 'Junior Software Developer Co-op',
+    date: 'September - December 2021',
+    bullets: ['Something about Mach85', 'Something else about Mach85'],
+  },
+  {
+    id: 1,
+    company: 'Populus Global Solutions',
+    href: 'https://www.populusplus.com/',
+    title: 'Full Stack Developer Co-op',
+    date: 'May - August 2022',
+    bullets: [
+      "Developed several key features of the company's web app product from front to back using Angular and Spring Boot.",
+      'Created unit and integration tests for my features, using Jest for Angular and JUnit for Spring Boot.',
+      'Used Angular paired with PrimeNG to craft intuitive front-end user experiences.',
+      'Leveraged Spring Boot with Java to construct a REST API, allowing users to perform various CRUD operations on several types of data.',
+      'Crafted SQL scripts to update and migrate data in a Postgres database.',
+      'Worked with a full Atlassian toolset to ensure our team abided to Agile development principles.',
+    ],
+  },
+].reverse();
+
+export default WORKS;
