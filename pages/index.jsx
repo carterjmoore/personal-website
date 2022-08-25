7a24a64d54c8367172b7bda0c5c910072da245f0
Carter Moore <carterjmoore02@gmail.com>
2022-08-25 00:01:29 -0300

    Add page title and favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,19 @@ import Footer from '../components/footer/Footer';
 import Home from '../components/home/Home';
 import Projects from '../components/projects/Projects';
 import Work from '../components/work/Work';
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Carter Moore&apos;s Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Carter Moore's personal portfolio website, built entirely from scratch."
+        />
+      </Head>
       <Home />
       <About />
       <Work />
