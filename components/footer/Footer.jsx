8b69e3a3ec8bd6ae68efcd4cc327a354b9857bba
Carter Moore <carterjmoore02@gmail.com>
2022-09-01 23:25:44 -0300

    Update footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,15 +1,23 @@
+import InlineLink from '../ui/InlineLink';
 import classes from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <div className={classes.footer}>
       <a
+        className={classes.built}
         href="https://github.com/carterjmoore/personal-website"
         target="_blank"
         rel="noopener noreferrer"
       >
         Built from scratch by Carter Moore
       </a>
+      <p className={classes.linkedIn}>
+        Get in touch through{' '}
+        <InlineLink href="https://www.linkedin.com/in/carterjmoore/">
+          LinkedIn
+        </InlineLink>
+      </p>
     </div>
   );
 };
